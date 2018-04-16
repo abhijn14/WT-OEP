9b11db4417cfa24b14cc2b82ff9c95ab0e14f37d
Abhi Jain <abhijn14@gmail.com>
2018-04-16 21:42:58 +0530

    All set

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var d = new Date();
 var n = d.getHours();
 var tT = document.querySelector(".topText");
 
-if(n>=4 && n<9)
+if(n>=4 && n<=9)
 {
     tT.style.background = "red";
     tT.textContent = "Sorry our services are unavailable at this moment. Please try after 9 AM. Thank you for your patience."
